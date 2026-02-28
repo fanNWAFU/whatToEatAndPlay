12f0793dfe8a59332d19dff22cea81b15cfcab59
fanNWAFU <fan1542455933@gmail.com>
2026-02-28 22:26:59 +0800

    newnew

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// 这是网页的“大脑”，一定要放在根目录的 vite.config.js 里！
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
